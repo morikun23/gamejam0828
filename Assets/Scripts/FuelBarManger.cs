300d42dcaa388fc5b31075d355312449e04556b7
DA\nwuser <kosyou555@gmail.com>
2017-08-28 19:11:36 +0900

    ・スコアの仮実装（スペースキーを押すと加算） ・燃料ゲージの実装（一定の値ずつ減り続ける使用）

---
+++ Assets/Scripts/FuelBarManger.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FuelBarManger : MonoBehaviour
+{
+    private Slider slider;  //燃料ゲージのスライダー
+
+    private float fuelValue = 100;  //燃料ゲージの値
+
+	// Use this for initialization
+	void Start ()
+    {
+        //スライダー(燃料ゲージ)を取得
+        slider = GetComponent<Slider>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        //燃料ゲージを減らす
+        fuelValue -= 0.5f;
+
+        slider.value = fuelValue;
+	}
+}
