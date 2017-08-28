b12a1ac5a3aa62f830e8024f11b35b0c4399b375
nwuser <nwuser@da.iwasaki.ac.jp>
2017-08-28 19:13:00 +0900

    背景スクロール 現状スクロールした際に画像と画像が切り替わるときに、若干の隙間ができてしまう。

---
+++ Assets/Scripts/scroll.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scroll : MonoBehaviour {
+    [SerializeField] //属性　これをつけることで変数にオプションを付けられる
+    scroll pair;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(-0.06f, 0, 0);
+        float width = GetComponent<SpriteRenderer>().bounds.size.x;
+        if (transform.position.x + -0.06f <= -width)
+        {
+            transform.position = new Vector3(width, 0, 0);
+            pair.transform.position = new Vector3(width, 0, 0);
+            pair.transform.position = transform.position - new Vector3(width / 2 + width / 2, 0, 0);
+        }
+	}
+}
