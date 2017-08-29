d5a6ffae82cc7179ff5df4bcae0f59e802976581
MoritaSuguru <morisug4645@gmail.com>
2017-08-29 09:20:40 +0900

    タイトルシーンの実装(仮)

---
+++ Assets/Scripts/TitleScene/TitleScene.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ToyBox;
+
+public class TitleScene : ToyBox.Scene {
+
+	bool isClicked;
+
+	public override void Start() {
+		base.Start();
+		isClicked = false;
+	}
+
+	public override string m_sceneName {
+		get { return "Title"; }
+	}
+
+	public override IEnumerator OnEnter() {
+		AppManager.Instance.m_fade.StartFade(new FadeIn() , Color.black , 0.5f);
+		yield return new WaitWhile(AppManager.Instance.m_fade.IsFading);
+	}
+
+	public override IEnumerator OnUpdate() {
+		while (true) {
+			if (isClicked) { break; }
+			yield return null;
+		}
+	}
+
+	public override IEnumerator OnExit() {
+		AppManager.Instance.m_fade.StartFade(new FadeOut() , Color.black , 0.5f);
+		yield return new WaitWhile(AppManager.Instance.m_fade.IsFading);
+		UnityEngine.SceneManagement.SceneManager.LoadScene("Game");
+	}
+
+	public void OnClick() {
+		isClicked = true;
+	}
+}
