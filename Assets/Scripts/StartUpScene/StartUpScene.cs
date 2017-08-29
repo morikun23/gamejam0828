aa9621a68c6734e723feb1a38300fc4d991acb36
MoritaSuguru <morisug4645@gmail.com>
2017-08-29 09:21:19 +0900

    スタートアップシーンの実装

---
+++ Assets/Scripts/StartUpScene/StartUpScene.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ToyBox;
+
+public class StartUpScene : Scene {
+
+	
+
+	public override string m_sceneName {
+		get { return "StartUp"; }
+	}
+
+	public override IEnumerator OnEnter() {
+		AppManager.Instance.m_fade.Fill(Color.black);
+		yield break;
+	}
+
+	public override IEnumerator OnUpdate() {
+		yield break;	
+	}
+
+	public override IEnumerator OnExit() {
+		UnityEngine.SceneManagement.SceneManager.LoadScene("Title");
+		yield return null;
+		
+	}
+}
